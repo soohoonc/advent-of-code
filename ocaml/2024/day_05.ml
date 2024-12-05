ae59e8425f4b8e5c9b2d2f6a26ed0a3544b36e1d
SooHoon Choi <soohoonchoi@gmail.com>
2024-12-05 10:07:07 -0800

    simplify parsing

--- ocaml/2024/day_05.ml
+++ ocaml/2024/day_05.ml
@@ -3,40 +3,33 @@
 module IntMap = Map.Make (Int)
 
 let parse input =
-  let lines = Core.String.split_on_chars ~on:[ '\n' ] input in
-  let rec parse_rules acc idx =
-    if idx >= List.length lines || String.trim (List.nth lines idx) = "" then
-      (acc, idx + 1)
-    else
-      let line = List.nth lines idx in
-      match Core.String.split_on_chars ~on:[ '|' ] line with
-      | [ num1; num2 ] ->
-          let n1 = int_of_string num1 and n2 = int_of_string num2 in
-          let existing =
-            match IntMap.find_opt n1 acc with Some l -> l | None -> []
-          in
-          let new_acc = IntMap.(add n1 (n2 :: existing) acc) in
-          parse_rules new_acc (idx + 1)
-      | _ -> parse_rules acc (idx + 1)
+  let rules, pages =
+    Core.String.split_on_chars ~on:[ '\n'; '\r' ] input
+    |> List.filter (fun s -> String.trim s <> "")
+    |> List.fold_left
+         (fun (rules, pages) line ->
+           match Core.String.split_on_chars ~on:[ '|' ] line with
+           | [ n1; n2 ] ->
+               let n1 = int_of_string n1 and n2 = int_of_string n2 in
+               let rules' =
+                 IntMap.update n1
+                   (function None -> Some [ n2 ] | Some l -> Some (n2 :: l))
+                   rules
+               in
+               (rules', pages)
+           | _ ->
+               let page =
+                 Core.String.split_on_chars ~on:[ ',' ] line
+                 |> List.map int_of_string
+               in
+               (rules, page :: pages))
+         (IntMap.empty, [])
   in
-  let rec parse_pages idx acc =
-    if idx >= List.length lines then acc
-    else
-      let line = List.nth lines idx in
-      if String.trim line = "" then acc
-      else
-        let page_list =
-          Core.String.split_on_chars ~on:[ ',' ] line |> List.map int_of_string
-        in
-        parse_pages (idx + 1) (page_list :: acc)
-  in
-  let rules, index = parse_rules IntMap.empty 0 in
-  let pages = parse_pages index [] |> List.rev in
-  (rules, pages)
+  (rules, List.rev pages)
 
 let mid page_list = List.nth page_list (List.length page_list / 2)
 
-let sort unsorted_pages rules =
+let sorted unsorted_pages rules =
   let compare_with_rules a b =
     match IntMap.find_opt a rules with
     | Some rule_list when List.mem b rule_list -> -1
@@ -52,7 +45,7 @@ let solve_part_one input =
   let rec pages_loop acc = function
     | [] -> acc
     | page :: rest ->
-        if sort page rules = page then pages_loop (acc + mid page) rest
+        if sorted page rules = page then pages_loop (acc + mid page) rest
         else pages_loop acc rest
   in
   pages_loop 0 pages
@@ -62,8 +55,8 @@ let solve_part_two input =
   let rec pages_loop acc = function
     | [] -> acc
     | page :: rest ->
-        if not (sort page rules = page) then
-          pages_loop (acc + mid (sort page rules)) rest
+        if sorted page rules <> page then
+          pages_loop (acc + mid (sorted page rules)) rest
         else pages_loop acc rest
   in
   pages_loop 0 pages
